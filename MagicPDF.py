fe70dd53f7cf5140d3601e11488938afc2817bf8
Silvos90 <aldo.mollica@gmail.com>
2025-08-17 16:41:44 +0200

    add pyproject.toml and update window title with funct name

--- MagicPDF.py
+++ MagicPDF.py
@@ -81,18 +81,22 @@ def split():
 
 def show_compress_frame():
     compress_frame.pack(fill='both', expand=True)
+    root.title("Magic PDF - Compress")
     forget_all_frames(root, compress_frame)
 
 def show_rotate_frame():
     rotate_frame.pack(fill='both', expand=True)
+    root.title("Magic PDF - Rotate")
     forget_all_frames(root, rotate_frame)
 
 def show_merge_frame():
     merge_frame.pack(fill='both', expand=True)
+    root.title("Magic PDF - Merge")
     forget_all_frames(root, merge_frame)
 
 def show_split_frame():
     split_frame.pack(fill='both', expand=True)
+    root.title("Magic PDF - Split")
     forget_all_frames(root, split_frame)
 
 def forget_all_frames(parent, current):
